5106da298645e94f641e0a6606b77f3e0c66a16e
Joseph Irwin <joseph.irwin.gt@gmail.com>
2025-12-14 07:01:54 +0900

    Day 4 Skeleton

---
+++ src/day-04/src/bin/part2.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::Read;
+
+use color_eyre::eyre::Result;
+
+use day_04::process_part2;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt::init();
+
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf)?;
+
+    let answer = process_part2(&buf)?;
+    println!("Answer for part 2: {answer}");
+
+    Ok(())
+}
---
+++ src/day-04/src/lib.rs
@@ -0,0 +1,22 @@
+use color_eyre::Result;
+
+pub fn process_part1(_input: &str) -> Result<usize> {
+    Ok(0)
+}
+
+pub fn process_part2(_input: &str) -> Result<usize> {
+    Ok(0)
+}
+
+fn _parse_input(_input: &str) -> Vec<Vec<u8>> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    const _TEST_INPUT: &str = "\
+987654321111111
+811111111111119
+234234234234278
+818181911112111";
+}
