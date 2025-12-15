7484bc17e0b47b2923a0fad5f8af0db4cbaba884
Joseph Irwin <joseph.irwin.gt@gmail.com>
2025-12-15 16:06:14 +0900

    day 4 - part 1 & 2

--- src/day-04/src/lib.rs
+++ src/day-04/src/lib.rs
@@ -7,11 +7,45 @@ pub fn process_part1(input: &str) -> Result<usize> {
         .lines()
         .map(|l| l.chars().map(|c| c == '@').collect())
         .collect();
-    Ok(0)
+    let nrows = rolls.len();
+    let ncols = rolls[0].len();
+
+    let count = (0..nrows)
+        .flat_map(|i| (0..ncols).map(move |j| (i, j)))
+        .filter(|&(i, j)| rolls[i][j] && neighbors(i, j, &rolls) < 4)
+        .count();
+
+    Ok(count)
 }
 
-pub fn process_part2(_input: &str) -> Result<usize> {
-    Ok(0)
+pub fn process_part2(input: &str) -> Result<usize> {
+    let mut rolls: Vec<Vec<bool>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c == '@').collect())
+        .collect();
+    let nrows = rolls.len();
+    let ncols = rolls[0].len();
+
+    let mut count = 0;
+
+    loop {
+        let available: Vec<(usize, usize)> = (0..nrows)
+            .flat_map(|i| (0..ncols).map(move |j| (i, j)))
+            .filter(|&(i, j)| rolls[i][j] && neighbors(i, j, &rolls) < 4)
+            .collect();
+
+        count += available.len();
+
+        for &(i, j) in available.iter() {
+            rolls[i][j] = false;
+        }
+
+        if available.len() == 0 {
+            break;
+        }
+    }
+
+    Ok(count)
 }
 
 fn _parse_input(_input: &str) -> Vec<Vec<u8>> {
@@ -36,7 +70,7 @@ fn neighbors(row: usize, col: usize, rolls: &Vec<Vec<bool>>) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::neighbors;
+    use super::{neighbors, process_part1, process_part2};
 
     const TEST_INPUT: &str = "\
 ..@@.@@@@.
@@ -58,5 +92,23 @@ mod tests {
             .collect();
 
         assert_eq!(neighbors(0, 0, &rolls), 2);
+        assert_eq!(neighbors(0, 1, &rolls), 4);
+        assert_eq!(neighbors(0, 2, &rolls), 3);
+        assert_eq!(neighbors(0, 7, &rolls), 4);
+        assert_eq!(neighbors(4, 4, &rolls), 8);
+
+        assert_eq!(neighbors(0, 9, &rolls), 3);
+        assert_eq!(neighbors(9, 0, &rolls), 1);
+        assert_eq!(neighbors(9, 9, &rolls), 2);
+    }
+
+    #[test]
+    fn test_process_part1() {
+        assert_eq!(process_part1(TEST_INPUT).unwrap(), 13);
+    }
+
+    #[test]
+    fn test_process_part2() {
+        assert_eq!(process_part2(TEST_INPUT).unwrap(), 43);
     }
 }
